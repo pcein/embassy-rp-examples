0e5a5d62eae7c8b7f31f3285a0c74601d19cd83e
Pramode <mail@pramode.in>
2025-08-17 15:45:44 +0530

    Run two tasks to blink two leds

--- src/bin/blink_task1.rs
+++ src/bin/blink_task1.rs
@@ -7,18 +7,21 @@ use embassy_time::{Duration, Timer};
 use panic_probe as _;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     // Initialize GPIO pin 25 as a push-pull output.
     // This is the built-in LED on the Pico board.
     let mut led = Output::new(p.PIN_25, Level::Low);
+    spawner.spawn(blink_led(led, Duration::from_millis(100)));
+}
 
+#[embassy_executor::task]
+async fn blink_led(mut led: Output<'static>, d: Duration) {
     loop {
         led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-
+        Timer::after(d).await;
         led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
+        Timer::after(d).await;
     }
 }
